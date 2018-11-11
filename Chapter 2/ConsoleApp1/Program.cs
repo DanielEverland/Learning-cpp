d83af88e4ede5b8ab3a5ad2bed82c34fd7189c3b
Daniel Everland <danieleverland@gmail.com>
2018-11-11 13:49:50 +0100

    Chapter 2

---
+++ Chapter 2/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int test;
+            int test1;
+            int test2;
+
+            test = test1 = test2 = 80;
+        }
+    }
+}
