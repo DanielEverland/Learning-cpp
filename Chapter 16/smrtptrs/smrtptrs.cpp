96ee698b384579e43181e38df51565be54da212a
Daniel Everland <danieleverland@gmail.com>
2019-06-15 12:08:47 +0200

    Chapter 16 progress

---
+++ Chapter 16/smrtptrs/smrtptrs.cpp
@@ -0,0 +1,31 @@
+// smrtptrs.cpp -- using three kinds of smart pointers
+#include <iostream>
+#include <string>
+#include <memory>
+
+class Report
+{
+public:
+	Report(const std::string s) : str(s) { std::cout << "Object created!\n"; }
+	~Report() { std::cout << "Object deleted!\n"; }
+	void comment() const { std::cout << str << std::endl; }
+
+private:
+	std::string str;
+};
+
+int main()
+{
+	{
+		std::auto_ptr<Report> ps(new Report("using auto_ptr"));
+		ps->comment();		// use -> to invoke a member function
+	}
+	{
+		std::shared_ptr<Report> ps(new Report("using shared_ptr"));
+		ps->comment();
+	}
+	{
+		std::unique_ptr<Report> ps(new Report("using unique_ptr"));
+		ps->comment();
+	}
+}
